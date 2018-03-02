b61f208d41954ccfc28b09ccdfbb4b5856e8898a
ange <abdjinan@yahoo.fr>
2018-03-02 11:16:35 +0100

    commit target ignore

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,10 +4,9 @@ package dev;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
