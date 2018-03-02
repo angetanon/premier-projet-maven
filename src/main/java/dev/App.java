030f4d975cd0b6aa3e6ec0ce626db9387c4f88f6
ange <abdjinan@yahoo.fr>
2018-03-02 12:46:43 +0100

    maven projet 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,12 +1,17 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
+
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		String asciiArt = FigletFont.convertOneLine("hello");
+		System.out.println(asciiArt);
 	}
 
 }
